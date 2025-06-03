d17a1c3d6e2921bad1ce79a4cc2a8ffb0f557c15
Luan Ibanhez <luanibanhez@gmail.com>
2025-06-03 12:59:28 -0300

    lib baixar pdf atualizado

--- tests/Feature/RoteTest.php
+++ tests/Feature/RoteTest.php
@@ -1,6 +1,6 @@
 <?php
 
-test('example', function () {
+it('testa se qualquer usuario pode acessar a rota para visualizar o livro', function () {
     $response = $this->get('/');
 
     $response->assertStatus(200);
